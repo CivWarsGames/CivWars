0be495f0b69ca4954d495d18189edbc0993b9356
z4m0 <martizzcc@gmail.com>
2010-11-16 17:44:33 +0100

    Added class User Added class Server (it contains the server defaults)

--- servers/Server.php
+++ servers/Server.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ * 
+ * The vars of a server with its defaults
+ *
+ */
+class Server
+{
+	public $dbHost = 'localhost';
+	public $dbUser = 'root';
+	public $dbPass = 'root';
+	public $dbName = 'test';
+	public $dbPrefix = '';
+	public $sName = 'sX';
+	public $maxUsers = -1; // -1 = unlimited
+	public $appRoot = '';//The root of the aplication ex: GameServer
+	public $serverSpeedRate = 1; //The speed of the server It must be > 0!!!
+	public $allowedGameType = 'ALL';// A String of the allowed themes separated by commas if 'ALL' is the first the following are blacklist
+	public $hidden = false; //if true the server is not showed to the users
+	
+	public function __construct(){}
+}
+
+?>
