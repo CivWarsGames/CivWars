481961cb1075c57b0afe384aca2112ca64dbba4b
z4m0 <martizzcc@gmail.com>
2010-11-17 20:55:12 +0100

    Removed to wrong placed files and updated the main game file

--- GameServerV2/Play.php
+++ GameServerV2/Play.php
@@ -1,1 +1,37 @@
 <?php
+if(!defined('APP')){
+require_once 'pathBuilder.php';
+}
+require_once APP.'logic/Session.php';
+require_once APP.'datamanager/serverSettings.php';
+require_once APP.'logic/Got.php';
+/**
+ * 
+ * Launches all the proces and selects the data to show
+ *
+ */
+class Play
+{
+	private  static $_session;
+	
+	public static function get_session()
+	{
+		return self::$_session;
+	}
+	
+	public static function  launch()
+	{
+		self::$_session = new Session();
+		if(self::$_session->get_sessionState() == true){
+			$user = self::$_session->user;
+			$user->update();
+		}else{
+			echo self::$_session->loginFailMessage;
+			//TODO redirect a non logged user
+		}
+		
+		Got::handleGetParams();
+	}
+}
+
+?>
