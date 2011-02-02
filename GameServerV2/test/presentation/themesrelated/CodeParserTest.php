6f809aad023573b0950322f0bdaafec4cb4dfaba
z4m0 <martizzcc@gmail.com>
2011-02-02 15:03:08 +0100

    Some identations

--- GameServerV2/test/presentation/themesrelated/CodeParserTest.php
+++ GameServerV2/test/presentation/themesrelated/CodeParserTest.php
@@ -1,19 +1,19 @@
 <?php
 if(!defined('APP')){
-	require_once '../../../pathBuilder.php';
+    require_once '../../../pathBuilder.php';
 }
 require_once APP."presentation/themesrelated/CodeParser.php";
 
 class CodeParserTest extends CodeParser
 {
-	public function CodeParserTest($filename){
-		if(!isset($_GET['t'])) $_GET['t'] = 'testtheme1';
-		$this->tplRoot = HOME."themes/testtheme1/";
+    public function CodeParserTest($filename){
+        if(!isset($_GET['t'])) $_GET['t'] = 'testtheme1';
+        $this->tplRoot = HOME."themes/testtheme1/";
 
-		$code = $this->getFileContent($filename);
-		echo '<textarea rows="36" cols="160" readonly style="background-color:#efffef">'.
-		$this->parseContent($code).'</textarea>';
-	}
+        $code = $this->getFileContent($filename);
+        echo '<textarea rows="36" cols="160" readonly style="background-color:#efffef">'.
+        $this->parseContent($code).'</textarea>';
+    }
 }
 
 
--- GameServerV2/test/presentation/themesrelated/CompilerTest.php
+++ GameServerV2/test/presentation/themesrelated/CompilerTest.php
@@ -3,21 +3,21 @@
  * This is a compiler test
  */
 if(!defined('APP')){
-	require_once '../../../pathBuilder.php';
+    require_once '../../../pathBuilder.php';
 }
 require_once APP."presentation/themesrelated/Compiler.php";
 
 class CompilerTest
 {
-	public static function test($theme)
-	{
-		$compiler = new Compiler($theme);
-		$code = self::returnCode(HOME.'themes/'.$theme.'/php/'.$_GET['p']);
-		echo '<textarea rows="36" cols="160" readonly style="background-color:#efffef">'.$code."</textarea>";
-	}
-	public static function returnCode($path){
-		return file_get_contents($path);		
-	}	
+    public static function test($theme)
+    {
+        $compiler = new Compiler($theme);
+        $code = self::returnCode(HOME.'themes/'.$theme.'/php/'.$_GET['p']);
+        echo '<textarea rows="36" cols="160" readonly style="background-color:#efffef">'.$code."</textarea>";
+    }
+    public static function returnCode($path){
+        return file_get_contents($path);
+    }
 }
 CompilerTest::test("testtheme1");
 
