7423964e629ef2f6ef5fe0fc3bab3e2239eb810e
z4m0 <martizzcc@gmail.com>
2011-03-23 19:53:14 +0100

    Added some buildings groups

--- GameServerV2/presentation/groups/CurrentUpgrades.php
+++ GameServerV2/presentation/groups/CurrentUpgrades.php
@@ -21,13 +21,14 @@ class CurrentUpgrades
 
         if($info['current_improvement'] == "defences") $buildings['NAME'] = $names[16];
         else $buildings['NAME'] = $names[$info['type_'.$info['current_improvement']]];
-        if($buildings['NAME'] == "PLOT") $buildings['NAME'] = 0;
+        if($buildings['NAME'] == "") $buildings['NAME'] = 0;
         else{
             $buildings['TO_LEVEL'] = $info['level_'.$info['current_improvement']]+1;
             $buildings['FINISH_TIME'][0] = $info['finish_time'];
             $buildings['FINISH_TIME'][1] = Timer::uNIXToDate($buildings['FINISH_TIME'][0]);
             $buildings['TIME_TO_FINISH'][0] = $buildings['FINISH_TIME'][0] - time();
             $buildings['TIME_TO_FINISH'][1]  =  Timer::secondsToDate($buildings['TIME_TO_FINISH'][0]);
+            
         }
         
         self::$upgrades['BUILDINGS'] = $buildings;
@@ -37,7 +38,7 @@ class CurrentUpgrades
         $info1 = DataBaseManager::fetchArray(DataBaseManager::query("SELECT mat_box_finish_time, mat_box_id_cur_improvement FROM {materials}
         WHERE city_id = ".User::get_currentCityId()));
         $info2 = DataBaseManager::fetchArray(DataBaseManager::query("SELECT box_level,current_type FROM {map} WHERE box_id = ".$info1['mat_box_id_cur_improvement']));
-
+        
         LoadMaterialsCosts::getmaterialNames(User::get_faction());
         $names = &LoadMaterialsCosts::$materialNames;
         if($info1['mat_box_id_cur_improvement'] == 0) $materials['NAME'] = 0;
